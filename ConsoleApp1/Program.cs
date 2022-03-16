bbe57e6f4b7987fdf1bdf4bc2aa1eae397b5daf4
Mia Peljto <73286005+peljto@users.noreply.github.com>
2022-03-16 17:37:46 +0100

    Mia message

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Novo");
+            Console.WriteLine("nikad popijene pive...");
         }
     }
 }
